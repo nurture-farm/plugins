d4467b750b487999015d5dbba8b0d7e093d34d68
debojyoti <debojyoti.karmakar@neebal.com>
2024-01-04 10:47:14 +0530

    Refactor

--- packages/camera/camera/lib/camera.dart
+++ packages/camera/camera/lib/camera.dart
@@ -13,7 +13,8 @@ export 'package:camera_platform_interface/camera_platform_interface.dart'
         ResolutionPreset,
         XFile,
         ImageFormatGroup,
-        BarcodeFormat;
+        BarcodeFormat,
+        Barcode;
 
 export 'src/camera_controller.dart';
 export 'src/camera_image.dart';
